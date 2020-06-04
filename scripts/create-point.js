9b18bddbd82896a50e38400914543549f5db1f54
Gustavo <gnkenshin@gmail.com>
2020-06-04 18:11:46 -0300

    Aula 3 parte 1

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -9,18 +9,48 @@ function selectFill(url, element){
 }
 
 function populaStates(){
-    const ufSelect = document.querySelector("select[name = state]");
-    let url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
-    selectFill(url, ufSelect);
+    const ufSelect = document.querySelector("select[name = state]")
+    let url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+    selectFill(url, ufSelect)
 }
 
 function populaCity(event){
-    const citySelect = document.querySelector('select[name = city');
-    let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/municipios`;
-    citySelect.innerHTML = `<option value="">Selecione a Cidade</option>`;
-    selectFill(url, citySelect);
-    citySelect.disabled = false;
+    const citySelect = document.querySelector('select[name = city]')
+    const ufSelect = document.querySelector('input[name = uf]')
+    const ufvalue = event.target.selectedIndex
+    ufSelect.value = event.target.options[ufvalue]
+    let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/municipios`
+    citySelect.innerHTML = `<option value="">Selecione a Cidade</option>`
+    citySelect.disabled = true
+    selectFill(url, citySelect)
+    citySelect.disabled = false
 }
 
-populaStates();
-document.querySelector('select[name = state]').addEventListener("change", populaCity);
+populaStates()
+document.querySelector('select[name = state]').addEventListener("change", populaCity)
+
+const itemToColect = document.querySelectorAll('.itens-grind li')
+for(let item of itemToColect){
+    item.addEventListener('click', handSelectedItem)
+}
+
+const colectedItens = document.querySelector('input[name=itens]')
+let selectedItens = []
+
+function handSelectedItem(event){
+    const item = event.target
+    item.classList.toggle("selected")
+    const itemID = item.dataset.id
+    const alreadSelected = selectedItens.findIndex(itemselected => {
+        return itemID == itemselected
+    })
+    if(alreadSelected >= 0){
+        const filtredItens = selectedItens.filter(itemSelected =>{
+            return itemID != itemSelected
+        })
+        selectedItens = filtredItens
+    }else{
+        selectedItens.push(itemID)
+    }
+    colectedItens.value = selectedItens
+}
